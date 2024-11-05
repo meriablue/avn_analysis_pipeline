4b0112f351eaa14e3c3996784d856299a1dc00e8
meriablue <maria.pescaru@mail.mcgill.ca>
2024-11-05 17:33:35 -0500

    upload similarity scripts

---
+++ self_similarity.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 16:37:40 2024
+
+@author: Sakata-Woolley
+"""
+
+import avn.similarity as similarity
+import pandas as pd
+import os
+import csv
+from itertools import combinations
+
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+import random 
+
+
+def get_seg_file(song_folder):
+    '''
+    Function takes as input a song_directory and returns path to segmentation file
+    :param song_folder: path to directory with .wav files and segmentation
+    :return: string
+    '''
+    parent_directory, directory_name = os.path.split(song_folder)
+    segmentations_dir = os.path.join(song_folder, 'Segmentations')
+    seg_file_path = os.path.join(segmentations_dir, f"{directory_name}.csv")
+    print(song_folder+'\n' + seg_file_path+'\n'+directory_name+'\n')
+    return seg_file_path, directory_name
+
+
+def spect_prep(song_folder):
+    '''
+    Function takes as input the path to a song folder and splits the original 
+    Segmentation file into Segmentation files for each song for further processing
+    '''
+    seg_file_path, bird_id = get_seg_file(song_folder)
+    segmentations = pd.read_csv(seg_file_path)
+    segmentations.head()
+    out_dir = os.path.join(song_folder, 'Similarity_Prep')
+    grouped = segmentations.groupby('files')
+    print(grouped.groups.keys())
+    
+    for file in os.listdir(song_folder):
+        if file.endswith('.wav') and file in grouped.groups.keys():                                                           
+            seg_file_one_song = grouped.get_group(file)
+            out_dir_one_file = os.path.join(out_dir, file)
+            song_folder = song_folder+'\\'
+            similarity.prep_spects(Bird_ID=bird_id, segmentations=seg_file_one_song, song_folder_path=song_folder,
+                               out_dir=out_dir_one_file)
+    return out_dir
+        
+
+def similarity_score(song_folder, output_dir):
+    '''
+    Main function of program, creates a csv file in the output directory 
+    containing 1000 EMD scores for random subsets of the spectrograms
+    :param song_folder: folder with .wav files and segmentation
+    :return:string: path to similarity prep folder
+    '''
+    #Create csv file
+    parent_directory, dir_name = os.path.split(song_folder)
+    csv_file_path = os.path.join(output_dir, f"{dir_name}.csv")
+    
+    #calculate embeddings
+    model=similarity.load_model()
+    spect_dir=spect_prep(song_folder)
+    embeddings=[]
+    i = 1; 
+    all_syllables = 0; 
+    
+    for subdir in os.listdir(spect_dir):
+        spect_dir_one_file = os.path.join(spect_dir, subdir)
+        if os.path.isdir(spect_dir_one_file):
+            print("___________"+spect_dir_one_file+"____________")
+            embeddings_one_file = similarity.calc_embeddings(Bird_ID = dir_name, 
+                                                         spectrograms_dir = spect_dir_one_file, 
+                                                         model=model)
+            embeddings.append(embeddings_one_file)
+            all_syllables = all_syllables + embeddings_one_file.shape[0]
+            print("EMBEDDINGS SHAPE:", embeddings_one_file.shape)
+            #embeddings = similarity.calc_embeddings(Bird_ID = dir_name, 
+                                            #spectrograms_dir=spect_dir,
+                                            #model=model)
+            i=i+1
+    
+    nb_files = i - 1
+    
+    index_size = int(np.floor(nb_files/2))
+    
+        
+    
+    
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow([song_folder])
+        csv_writer.writerow(['', 'EMD'])
+        indices = list(range(0, nb_files))
+       
+        #generate splits
+        for i in range(1000):
+            
+            
+            #print("INDEX ARRAY INDEX ARRAY INDEX ARRAY")
+
+            #print(indices)
+            random.shuffle(indices)
+            
+            split1_indices = np.random.choice(indices, index_size, replace=False)
+            split2_indices = np.setdiff1d(indices, split1_indices)
+            
+            split_size = int(np.floor(all_syllables/2))
+            split1 = np.empty((split_size, 8))
+            split2 = np.empty((split_size, 8))
+            
+            
+            counter_split1 = 0
+            counter_split2 = 0
+            
+            #print("INDEX ARRAYS BEING COMPARED\n\n\n")
+            #print(split1_indices)
+            #print(split2_indices)
+            
+            for j in range(nb_files-1):
+                embeddings_one_file = np.copy(embeddings[j])
+                (nb_syll_one_file, nb_emb) = embeddings_one_file.shape
+                if j in split1_indices:
+                    
+                    for k in range (nb_syll_one_file - 1):
+                        if counter_split1 < split_size : 
+                            #print (embeddings_one_file)
+                            #print("NOW SUPOPSEDLY ONE ROW")
+                            #print (embeddings_one_file[k])
+                            split1[counter_split1] = np.copy(embeddings_one_file[k])
+                            counter_split1 = counter_split1 + 1 
+                    
+                else:
+                    
+                    for k in range (nb_syll_one_file-1):
+                        if counter_split2 < split_size : 
+                            #print (embeddings_one_file)
+                            #print("NOW SUPOPSEDLY ONE ROW")
+                            #print (embeddings_one_file[k])
+                            split2[counter_split2] = np.copy(embeddings_one_file[k])
+                            counter_split2 = counter_split2 + 1
+           
+            emd = similarity.calc_emd(split1, split2)
+            
+            print("EMD SCORE:", emd)
+            
+            csv_writer.writerow([i, emd])
+            
+            
+if __name__ == "__main__":
+
+            
+#Parameters to change:
+
+    song_directory = r'C:\Users\Sakata-Woolley\DATA\pipeline_test'
+    output_directory = r'C:\Users\Sakata-Woolley\DATA\pipeline_test_ouput'
+    
+   #for subdir in os.listdir(song_directory):
+        
+    #   subdir_path = os.path.join(song_directory, subdir)
+        
+    for subdir in os.listdir(song_directory):
+                
+                sub_path = os.path.join(song_directory, subdir)
+                similarity_score(sub_path, output_directory)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ similarity_scoring.py
@@ -0,0 +1,157 @@
+import avn.similarity as similarity
+import pandas as pd
+import os
+import csv
+from itertools import combinations
+
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_seg_file(song_folder):
+    '''
+    Function takes as input a song_directory and returns path to segmentation file
+    :param song_folder: path to directory with .wav files and segmentation
+    :return: string
+    '''
+    parent_directory, directory_name = os.path.split(song_folder)
+    segmentations_dir = os.path.join(song_folder, 'Segmentations')
+    seg_file_path = os.path.join(segmentations_dir, f"{directory_name}.csv")
+    print(song_folder+'\n' + seg_file_path+'\n'+directory_name+'\n')
+    return seg_file_path, directory_name
+
+
+def calculate_embeddings(song_folder, embeddings_arr):
+    '''
+    Function stores embeddings and stores them in embeddings_arr
+    :param song_folder: folder with .wav files and segmentation
+    :param embeddings_arr: array of embeddings that will be populated
+    :return:
+    '''
+    # Get the segmentation files and bird ID for each of the birds
+    seg_file_path, bird_id = get_seg_file(song_folder)
+    
+    
+
+    segmentations = pd.read_csv(seg_file_path)
+    segmentations.head()
+    out_dir = os.path.join(song_folder, 'Similarity_Prep')
+    song_folder = song_folder+'\\'
+    similarity.prep_spects(Bird_ID=bird_id, segmentations=segmentations, song_folder_path=song_folder,
+                           out_dir=out_dir)
+    model = similarity.load_model()
+    embeddings_arr = similarity.calc_embeddings(Bird_ID=bird_id, spectrograms_dir=out_dir, model=model)
+
+
+def similarity_scoring(song_folder_1, song_folder_2):
+    '''
+    Function computes similarity scores for 2 birds and returns emd score
+    :param song_folder_1: path to .wav and segmentation folder of bird 1
+    :param song_folder_2: path to .wav and segmentation folder of bird 2
+    :return: float
+    '''
+
+    #Get embeddings for each bird
+    embeddings_1 = np.array([[]])
+    embeddings_2 = np.array([[]])
+   
+    
+    #Calculate embeddings song 1
+    seg_file_path, bird_id = get_seg_file(song_folder_1)
+
+    segmentations = pd.read_csv(seg_file_path)
+    segmentations.head()
+    out_dir = os.path.join(song_folder_1, 'Similarity_Prep')
+    song_folder = song_folder_1+'\\'
+    similarity.prep_spects(Bird_ID=bird_id, segmentations=segmentations, song_folder_path=song_folder,
+                           out_dir=out_dir)
+    model = similarity.load_model()
+    embeddings_1 = similarity.calc_embeddings(Bird_ID=bird_id, spectrograms_dir=out_dir, model=model)
+    
+    
+    #Caluclate embeddinghs song 2 
+    seg_file_path, bird_id = get_seg_file(song_folder_2)
+
+    segmentations = pd.read_csv(seg_file_path)
+    segmentations.head()
+    out_dir = os.path.join(song_folder_2, 'Similarity_Prep')
+    song_folder = song_folder_2+'\\'
+    similarity.prep_spects(Bird_ID=bird_id, segmentations=segmentations, song_folder_path=song_folder,
+                           out_dir=out_dir)
+    model = similarity.load_model()
+    embeddings_2 = similarity.calc_embeddings(Bird_ID=bird_id, spectrograms_dir=out_dir, model=model)
+    
+
+    #Calculate emd score
+    emd = similarity.calc_emd(embeddings_1, embeddings_2)
+    print("________")
+    print(embeddings_1)
+    return emd
+
+
+def similarity_scoring_directory(dir_path, output_dir_path):
+    '''
+    Function creates a .csv file in the output directory with comparisons
+    between all birds in given directory
+    :param bird_dir_path: Path to directory containing subdirectories with .wav files
+    :param output_dir_path: Path to directory where output .csv file will be saved
+    :return: noneo
+    '''
+
+    #Create csv file
+    parent_directory, dir_name = os.path.split(dir_path)
+    csv_file_path = os.path.join(output_dir_path, f"{dir_name}.csv")
+
+    #get all subdirectories
+    subdirectories = [os.path.join(dir_path, d) for d in os.listdir(dir_path) if
+                      os.path.isdir(os.path.join(dir_path, d))]
+    print(subdirectories)
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Bird1', 'Bird2', 'SimilarityScore'])
+
+        for dir1, dir2 in combinations(subdirectories, 2):
+            print("Calling similarity_scoring on", dir1, dir2)
+            score = similarity_scoring(dir1, dir2)
+            subdir1_name = os.path.basename(dir1)
+            subdir2_name = os.path.basename(dir2)
+            csv_writer.writerow([subdir1_name, subdir2_name, score])
+
+    print(f"CSV file created at: {csv_file_path}")
+
+
+if __name__ == "__main__":
+
+    #parameters to change
+
+    
+    #Make parameter 1 if you want to use for self-similarity scoring, 0 if large folder with multiple rounds
+    sim_single = 0
+
+    #Parent directory with all rounds (for sim between rounds) or bird to self-compare
+    #Parent directory should contain rounds as subdirectories 
+    directory_path = r'C:\Users\Sakata-Woolley\Desktop\olivia\learnability'
+
+    #Directory where comparison for each folder will be saved in a separate csv file
+    output_path = r'C:\Users\Sakata-Woolley\Desktop\olivia\learnability_output\similarity_scoring'
+    
+    if sim_single : 
+        
+        for subdirectory in os.listdir(directory_path):
+            if subdirectory[0] != '.' and subdirectory[0] != 'M':
+                filecomp_path = os.path.join(directory_path, subdirectory, "FileComp", subdirectory)
+                similarity_scoring_directory(filecomp_path, output_path)
+        
+    else:
+    #Loop through subdirectories and call similarity_scoring
+        #similarity_scoring_directory(directory_path, output_path)
+
+        for subdirectory in os.listdir(directory_path):
+            if subdirectory[0] != '.':
+                print("______"+subdirectory+"_______")
+                print("Calling similarity_scoring_directory on", subdirectory)
+                subdirectory_path = os.path.join(directory_path, subdirectory)  
+                similarity_scoring_directory(subdirectory_path, output_path)
+
+
